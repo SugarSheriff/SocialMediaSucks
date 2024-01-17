6c8a601af77f20521a4b3d91698cd3846872dad7
lchulsey <126140963+SugarSheriff@users.noreply.github.com>
2024-01-16 19:28:07 -0600

    Pages

---
+++ client/src/components/Header/header.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>My Social Media Sucks</h1>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,5 @@
-// src/index.js
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Assuming you have an App component
 
 ReactDOM.render(
   <React.StrictMode>
